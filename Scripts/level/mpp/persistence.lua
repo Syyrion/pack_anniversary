a32e09dcd97e4f2c77ab64229234cfbdbd1320f4
someNesa <sjoe6291@gmail.com>
2022-10-13 11:08:11 -0700

    more cw

--- Scripts/level/mpp/persistence.lua
+++ Scripts/level/mpp/persistence.lua
@@ -42,21 +42,21 @@ pastRad = 0
 
 --cw decoration
 
---layer 1
+--layer 1 - ring
 cw11 = cw_createNoCollision()
 cw12 = cw_createNoCollision()
 cw13 = cw_createNoCollision()
 cw14 = cw_createNoCollision()
 cw15 = cw_createNoCollision()
 cw16 = cw_createNoCollision()
---layer 2
+--layer 2 - ends
 cw21 = cw_createNoCollision()
 cw22 = cw_createNoCollision()
 cw23 = cw_createNoCollision()
 cw24 = cw_createNoCollision()
 cw25 = cw_createNoCollision()
 cw26 = cw_createNoCollision()
---layer 3
+--layer 3 - cross
 cw31 = cw_createNoCollision()
 cw32 = cw_createNoCollision()
 cw33 = cw_createNoCollision()
@@ -65,12 +65,39 @@ cw35 = cw_createNoCollision()
 cw36 = cw_createNoCollision()
 
 function cwspawnwalllayer1(cw)
-	local length = 375
-	local width = 50
+	local length = 342
+	local width = 10
 	local displacement = 600
 	local alpha = 200
-    cw_setVertexPos(cw, 0, displacement+width, length)
-    cw_setVertexPos(cw, 1, displacement+width, 0-length)
+	local conical = 10
+    cw_setVertexPos(cw, 0, displacement+width, length+conical)
+    cw_setVertexPos(cw, 1, displacement+width, 0-length-conical)
+    cw_setVertexPos(cw, 2, displacement-width, 0-length)
+    cw_setVertexPos(cw, 3, displacement-width, length)
+    cw_setVertexColor4Same(cw, alpha, alpha, alpha, 255)
+end
+
+function cwspawnwalllayer2(cw)
+	local length = 40
+	local width = 1
+	local displacement = 350
+	local alpha = 200
+	local conical = 0
+    cw_setVertexPos(cw, 0, displacement+width, length+conical)
+    cw_setVertexPos(cw, 1, displacement+width, 0-length-conical)
+    cw_setVertexPos(cw, 2, displacement-width, 0-length)
+    cw_setVertexPos(cw, 3, displacement-width, length)
+    cw_setVertexColor4Same(cw, alpha, alpha, alpha, 255)
+end
+
+function cwspawnwalllayer3(cw)
+	local length = 0.5
+	local width = 600
+	local displacement = 900
+	local alpha = 200
+	local conical = 4
+    cw_setVertexPos(cw, 0, displacement+width, length+conical)
+    cw_setVertexPos(cw, 1, displacement+width, 0-length-conical)
     cw_setVertexPos(cw, 2, displacement-width, 0-length)
     cw_setVertexPos(cw, 3, displacement-width, length)
     cw_setVertexColor4Same(cw, alpha, alpha, alpha, 255)
@@ -88,17 +115,40 @@ function cwspawn()
 	cwspin(cw14, (math.pi/3)*3)
 	cwspin(cw15, (math.pi/3)*4)
 	cwspin(cw16, (math.pi/3)*5)
+	cwspawnwalllayer2(cw21)
+	cwspawnwalllayer2(cw22)
+	cwspawnwalllayer2(cw23)
+	cwspawnwalllayer2(cw24)
+	cwspawnwalllayer2(cw25)
+	cwspawnwalllayer2(cw26)
+	cwspin(cw22, (math.pi/3))
+	cwspin(cw23, (math.pi/3)*2)
+	cwspin(cw24, (math.pi/3)*3)
+	cwspin(cw25, (math.pi/3)*4)
+	cwspin(cw26, (math.pi/3)*5)
+	cwspawnwalllayer3(cw31)
+	cwspawnwalllayer3(cw32)
+	cwspawnwalllayer3(cw33)
+	cwspawnwalllayer3(cw34)
+	cwspawnwalllayer3(cw35)
+	cwspawnwalllayer3(cw36)
+	cwspin(cw32, (math.pi/3))
+	cwspin(cw33, (math.pi/3)*2)
+	cwspin(cw34, (math.pi/3)*3)
+	cwspin(cw35, (math.pi/3)*4)
+	cwspin(cw36, (math.pi/3)*5)
 end
-
+pulsDist = 10
+pulsMin = 20
 function hardPulse()
     --the fast beat pulse
     l_setManualPulseControl(true)
-    l_setPulse(40)
+    l_setPulse(pulsMin)
     l_setManualPulseControl(false) -- i have no clue whether i actually need to reset the pulse each cycle
-    l_setPulseMin(40)
-    l_setPulseMax(80)
+    l_setPulseMin(pulsMin)
+    l_setPulseMax(pulsMin+pulsDist)
     l_setPulseSpeed(1200)
-    l_setPulseSpeedR((1200*BPM)/(108000-(BPM)))
+    l_setPulseSpeedR((pulsDist*1200*BPM)/(4320000-(pulsDist*BPM)))
     l_setPulseDelayMax(0)
 
 end
@@ -122,7 +172,7 @@ function onInit()
 	else
 		l_setSpeedMult(1.2)
 	end
-	l_setSpeedInc(0.125)
+	l_setSpeedInc(0.1)
 	l_setSpeedMax(3.5)
 	l_setRotationSpeed(10000)
 	l_setRotationSpeedMax(9999)
@@ -198,25 +248,44 @@ ibeatCount = 0
 -- `onUpdate` is an hardcoded function that is called every frame. `mFrameTime`
 -- represents the time delta between the current and previous frame.
 function onUpdate(mFrameTime)
+
+	--sustain cw decoration
 	theta = (l_getRotation()*math.pi)/180 - pastRad
+	if s_get3dSkew() < 0 then
+		theta = theta - 0.014
+	end
+	theta = theta + 0.007
 	cwspin(cw11, theta)
 	cwspin(cw12, theta)
 	cwspin(cw13, theta)
 	cwspin(cw14, theta)
 	cwspin(cw15, theta)
 	cwspin(cw16, theta)
+	cwspin(cw21, theta)
+	cwspin(cw22, theta)
+	cwspin(cw23, theta)
+	cwspin(cw24, theta)
+	cwspin(cw25, theta)
+	cwspin(cw26, theta)
+	cwspin(cw31, theta)
+	cwspin(cw32, theta)
+	cwspin(cw33, theta)
+	cwspin(cw34, theta)
+	cwspin(cw35, theta)
+	cwspin(cw36, theta)
 	pastRad = (l_getRotation()*math.pi)/180
+
+
 	if l_getLevelTime() > nextLoop then -- clock
         loopcount = loopcount + 1
         nextLoop = loopcount * (60/234) -- bpm = 234
         --on beat events
-		stopPulse(40)
 		hardPulse()
 		shdr_setActiveFragmentShader(RenderStage.BACKGROUNDTRIS, blackflash)
 		shdr_setActiveFragmentShader(RenderStage.PLAYERTRIS, blacks)
 		shdr_setActiveFragmentShader(RenderStage.PIVOTQUADS, blacks)
 		shdr_setActiveFragmentShader(RenderStage.CAPTRIS, blacks)
-		l_setRotationSpeed(1.0+(math.random() * 10.0))
+		l_setRotationSpeed(10.0+(math.random() * 100.0))
 
         beatCount = beatCount + 1
 		ibeatCount = 0
@@ -238,10 +307,10 @@ function onUpdate(mFrameTime)
 
 	FFrames = FFrames + 1
 	if dys then
-		s_set3dSkew(0.1)
+		s_set3dSkew(0.01)
 	else
 		if u_getDifficultyMult() >= 1.0000 then
-			s_set3dSkew(-2.1)
+			s_set3dSkew(-2.01)
 		end
 	end
 	ibeatCount = ibeatCount + 1
