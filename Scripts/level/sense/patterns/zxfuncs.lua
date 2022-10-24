a88aa11b72af7cc96a975c741ee3532486d5f3aa
zX <queballdqd@gmail.com>
2022-10-24 15:07:42 -0400

    Update zxfuncs.lua

--- Scripts/level/sense/patterns/zxfuncs.lua
+++ Scripts/level/sense/patterns/zxfuncs.lua
@@ -5,5 +5,33 @@ function randomWeighted(min, max, exponent)
 	return min + (max - min) * (math.random()^exponent)
 end
 
+function runCWs()
+	wallArrays(mFrameTime)
+
+	ArrayRemove(imposterWalls, function(t, i, j)
+		local v = t[i]
+		if v.dead then
+			cw_destroy(v.cwHandle)
+			return true
+		else
+			return false
+		end
+	end);
+	
+	ArrayRemove(bgWalls, function(t, i, j)
+		local v = t[i]
+		if v.dead then
+			cw_destroy(v.cwHandle)
+			return true
+		else
+			return false
+		end
+	end);
+	
+	-- Loop through all imposter walls to handle their movement
+	for _, iw in ipairs(imposterWalls) do
+		iw:move(mFrameTime)
+	end
+end
 
 
