54e7523b7ad6f51d2450d413d649cf668f17171a
zX <queballdqd@gmail.com>
2022-10-24 19:28:48 -0400

    Update zxfuncs.lua

--- Scripts/level/sense/patterns/zxfuncs.lua
+++ Scripts/level/sense/patterns/zxfuncs.lua
@@ -5,6 +5,25 @@ function randomWeighted(min, max, exponent)
 	return min + (max - min) * (math.random()^exponent)
 end
 
+function ArrayRemove(t, fnRemove)
+    local j, n = 1, #t;
+
+    for i=1,n do
+        if (not fnRemove(t, i, j)) then
+            -- Move i's kept value to j's position, if it's not already there.
+            if (i ~= j) then
+                t[j] = t[i];
+                t[i] = nil;
+            end
+            j = j + 1; -- Increment position of where we'll place the next kept value.
+        else
+            t[i] = nil;
+        end
+    end
+
+    return t;
+end
+
 function runCWs()
 	wallArrays(mFrameTime)
 
